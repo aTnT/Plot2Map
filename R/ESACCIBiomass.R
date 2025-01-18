8d13370621bfa49b5b09c4e718ed00bfa90d85d3
aTnT <atn.tavares@gmail.com>
2025-01-18 00:07:30 +0000

    download ESA CCI Biomass GeoTIFF data from the CEDA Archive

--- R/ESACCIBiomass.R
+++ R/ESACCIBiomass.R
@@ -7,8 +7,9 @@
 #' 2020, 2021 or "latest" (default).
 #' @param esacci_biomass_version The ESACCI BIOMASS AGB tiles version to use. Use either "v2.0", "v3.0", "v4.0",
 #' "v5.0", "v5.01" or "latest" (default).
-#' @param output_dir Directory to save downloaded files. Default is current working directory.
+#' @param output_dir Directory to save downloaded files. Default is "data/ESACCI-BIOMASS".
 #' @param n_cores Number of cores to use for parallel download. Default is all cores except one.
+#' @param timeout Number of seconds for reaching file download timeout.
 #' @param file_names Character vector of specific filenames to download. If NULL (default), all files will be downloaded.
 #'
 #' @return A character vector of downloaded file paths.
@@ -23,19 +24,14 @@ download_esacci_biomass <- function(esacci_biomass_year = "latest",
                                     esacci_biomass_version = "latest",
                                     output_dir = "data/ESACCI-BIOMASS",
                                     n_cores = parallel::detectCores() - 1,
+                                    timeout = 600,
                                     file_names = NULL) {
 
   base_url <- "https://data.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps"
 
-  # Validate and process input parameters
-  valid_years <- c(2010, 2015, 2016, 2017, 2018, 2019, 2020, 2021)
-  valid_versions <- c("v2.0", "v3.0", "v4.0", "v5.0", "v5.01")
-
-  if (esacci_biomass_year == "latest") esacci_biomass_year <- max(valid_years)
-  if (esacci_biomass_version == "latest") esacci_biomass_version <- valid_versions[length(valid_versions)]
-
-  if (!(esacci_biomass_year %in% valid_years)) stop("Invalid year specified")
-  if (!(esacci_biomass_version %in% valid_versions)) stop("Invalid version specified")
+  esacci_args <- validate_esacci_biomass_args(esacci_biomass_year, esacci_biomass_version)
+  esacci_biomass_year <- esacci_args$esacci_biomass_year
+  esacci_biomass_version <- esacci_args$esacci_biomass_version
 
   # Check if output directory exists, if not create it
   if (!dir.exists(output_dir)) {
@@ -55,7 +51,21 @@ download_esacci_biomass <- function(esacci_biomass_year = "latest",
   if (!is.null(file_names)) {
     file_names <- intersect(file_names, available_files)
     if (length(file_names) == 0) {
-      stop("None of the specified file names are available for download.")
+      if (esacci_biomass_version == "v5.01") {
+
+        # Create versions with 5.0 and 5.01
+        v5_0 <- gsub("-fv[0-9.]+", "-fv5.0", file_names)
+        v5_01 <- gsub("-fv[0-9.]+", "-fv5.01", file_names)
+
+        # Combine both versions
+        result <- c(v5_0, v5_01)
+
+        file_names <- intersect(result, available_files)
+
+      } else {
+        stop("None of the specified file names are available for download.")
+      }
+
     }
   } else {
     file_names <- available_files
@@ -63,15 +73,12 @@ download_esacci_biomass <- function(esacci_biomass_year = "latest",
 
   # Download function
   download_file <- function(file_name) {
+    options(timeout = max(timeout, getOption("timeout")))
+
     file_url <- file.path(url, file_name)
     output_file <- file.path(output_dir, file_name)
-    tryCatch({
-      download.file(file_url, output_file, mode = "wb", quiet = TRUE)
-      return(output_file)
-    }, error = function(e) {
-      warning(paste("Failed to download:", file_name, "-", e$message))
-      return(NULL)
-    })
+    download.file(file_url, output_file, mode = "wb", quiet = TRUE)
+    return(output_file)
   }
 
   message(paste0("Downloading ", length(file_names), " ESA CCI Biomass ", esacci_biomass_version, " file(s) for year ", esacci_biomass_year, "..."))
@@ -79,14 +86,90 @@ download_esacci_biomass <- function(esacci_biomass_year = "latest",
   # Setup parallel processing with progress bar
   cl <- makeCluster(n_cores)
 
-  # Parallel download with progress
-  downloaded_files <- pblapply(file_names, download_file, cl = cl)
+  # Parallel download with progress and error handling
+  downloaded_files <- pblapply(file_names, function(file_name) {
+    tryCatch({
+      download_file(file_name)
+    }, error = function(e) {
+      warning(paste("Failed to download:", file_name, "-", e$message))
+      return(file.path(output_dir, file_name))
+    })
+  }, cl = cl)
 
   stopCluster(cl)
 
-  # Remove NULL entries (failed downloads)
-  downloaded_files <- unlist(downloaded_files[!sapply(downloaded_files, is.null)])
+  # Check which files actually exist and return only those
+  downloaded_files <- unlist(downloaded_files)
+  existing_files <- downloaded_files[file.exists(downloaded_files)]
 
-  return(downloaded_files)
+  return(existing_files)
 }
+
+
+
+# Helper function used for args validation
+validate_esacci_biomass_args <- function (esacci_biomass_year, esacci_biomass_version) {
+
+  # Validate and process input parameters
+  valid_years <- c(2010, 2015, 2016, 2017, 2018, 2019, 2020, 2021)
+  valid_versions <- c("v2.0", "v3.0", "v4.0", "v5.0", "v5.01")
+  valid_years_v4 <- c(2010, 2017, 2018, 2019, 2020)
+  valid_years_v2_v3 <- c(2010, 2017, 2018)
+
+  if (esacci_biomass_year == "latest") esacci_biomass_year <- max(valid_years)
+  if (esacci_biomass_version == "latest") esacci_biomass_version <- valid_versions[length(valid_versions)]
+
+  if (!(esacci_biomass_year %in% valid_years)) stop("Invalid year specified, valid years: ", paste(valid_years, collapse = " "))
+  if (!(esacci_biomass_version %in% valid_versions)) stop("Invalid version specified")
+
+  if (esacci_biomass_version == "v2.0" | esacci_biomass_version == "v3.0") {
+    if (!(esacci_biomass_year %in% valid_years_v2_v3)) stop("Invalid year specified for v2.0 or v3.0, valid years: ", paste(valid_years_v2_v3, collapse = " "))
+  }
+
+  if (esacci_biomass_version == "v4.0") {
+    if (!(esacci_biomass_year %in% valid_years_v4)) stop("Invalid year specified for v4.0, valid years: ", paste(valid_years_v4, collapse = " "))
+  }
+
+  return(
+    list(
+      "esacci_biomass_year" = esacci_biomass_year,
+      "esacci_biomass_version" = esacci_biomass_version
+    )
+  )
+}
+
+
+
+
+
+
+
+
+# Tests:
+
+# test_that("AGBtileNames and download_esacci_biomass functions behaves consistently", {
+#
+#   # Amazon, 1 tile
+#   amazon_pol <- st_polygon(list(rbind(c(-62.2159, -3.4653), c(-62.2059, -3.4653),
+#                                       c(-62.2059, -3.4553), c(-62.2159, -3.4553),
+#                                       c(-62.2159, -3.4653))))
+#   amazon_pol_sf <- st_sfc(amazon_pol, crs = 4326)
+#
+#   # Mexico, 2 tiles
+#   mexico_pol <- st_polygon(list(rbind(c(-99.5,18), c(-101,19), c(-101,19), c(-99.5,19), c(-99.5,18))))
+#   mexico_pol_sf <- st_sfc(mexico_pol, crs = 4326)
+#
+#   result_amazon <- ESACCIAGBtileNames(amazon_pol_sf, esacci_biomass_year=2021, esacci_biomass_version="v5.01")
+#   result_mexico <- ESACCIAGBtileNames(mexico_pol_sf, esacci_biomass_year=2021, esacci_biomass_version="v5.01")
+#
+#   dwn_amazon <- download_esacci_biomass(file_names = result_amazon)
+#   dwn_mexico <- download_esacci_biomass(file_names = result_mexico)
+#
+#   expect_equal(dwn_amazon, "data/ESACCI-BIOMASS/N00W070_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2021-fv5.0.tif")
+#   expect_equal(dwn_mexico, c("data/ESACCI-BIOMASS/N20W110_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2021-fv5.0.tif",
+#                              "data/ESACCI-BIOMASS/N20W100_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2021-fv5.0.tif"))
+#
+# })
+
+
 
